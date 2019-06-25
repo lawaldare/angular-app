66e9440b6305841ed79679ae15164ea11bc66b49
Dare Lawal <lawaldare@gmail.com>
2019-06-25 14:38:59 +0100

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostService } from './services/post.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostComponent } from './components/post/post.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    NavbarComponent,
+    PostFormComponent,
+    HomeComponent,
+    UsersComponent,
+    PostComponent,
+    NotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
